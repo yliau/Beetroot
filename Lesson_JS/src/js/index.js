8af3320f8ca387e73f4955153dd4b3dd238ad74b
Yuliya <fedorova.yulia89@gmail.com>
2018-12-15 15:08:30 +0200

    cahee footer add

--- Lesson_JS/src/js/index.js
+++ Lesson_JS/src/js/index.js
@@ -65,5 +65,14 @@
 // 	return;
 // }
 
+let arr = [1, 2, 3];
 
+
+function square(x) { return x * x; } // возведение в квадрат
+console.log(map(square, [1, 2, 3, 4])); // [1, 4, 9, 16]
+console.log(map(square, [])); // []
+
+
+console.log(map(square, arr)); // [1, 4, 9]
+console.log(arr); // [1, 2, 3]
 
