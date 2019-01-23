faf1b97f54231b566ea3a1e1dcfed58d3a26a1d9
Yuliya <fedorova.yulia89@gmail.com>
2019-01-24 01:46:48 +0200

    exam2_map_add

--- exam2/src/js/script.js
+++ exam2/src/js/script.js
@@ -2,15 +2,12 @@
 	'use strict';
 	$(function(){
 		$('.team__slider').slick({
-			// dots: true,
 			slidesToScroll: 1,
 			infinite: true,
 			speed: 300,
 slidesToShow: 3,
 
-
-responsive: [
-   
+responsive: [   
     {
       breakpoint: 992,
       settings: {
@@ -24,11 +21,8 @@ responsive: [
         slidesToShow: 1,
         slidesToScroll: 1
       }
-    }
-    
-  ]
-
-			
+    }    
+  ]			
 		});
 	})
 })(jQuery);
@@ -38,25 +32,229 @@ form.on('submit', (e) => {
 	e.preventDefault();
 });
 
+$('.up').on('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+
+
 function initMap() {
 	let myLatLng = {lat: 49.062785, lng: 33.4156805};
 
+        let style = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c8c4da"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "color": "#f3f8f7"
+      },
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#b4aecc"
+      },
+      {
+        "weight": 2.5
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c8c4da"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c4c0d5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#c4c0d5"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#a49fae"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e1cd7b"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#c0a32c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "color": "#f4d335"
+      },
+      {
+        "lightness": 15
+      },
+      {
+        "weight": 2.5
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#827240"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c4c0d5"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#9eace5"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
         // Create a map object and specify the DOM element
         // for display.
         const map = new google.maps.Map(document.getElementById('map'), {
         	center: myLatLng,
-        	zoom: 7
+        	zoom: 15,
+        	scrollwheel: false,
+        	styles: style,
         });
 
+const image = 'img/map-marker.png';
 
         let marker = new google.maps.Marker({
         	map: map,
         	position: myLatLng,
-        	title: 'Hello World!'
+        	icon: image,
+        	
         });
+
+
+
+
     }
-
-
 
     $( () => {
 
@@ -70,15 +268,15 @@ function initMap() {
 	//Click Logo To Scroll To Top
 	$('#logo').on('click', () => {
 		$('html,body').animate({
-			scrollTop: 0
-		},500);
+			scrollTop: 0,
+		},4);
 	});
 	
 	//Smooth Scrolling Using Navigation Menu
 	$('a[href*="#"]').on('click', function(e){
 		$('html,body').animate({
 			scrollTop: $($(this).attr('href')).offset().top - 100
-		},500);
+		},4);
 		e.preventDefault();
 	});
 	
